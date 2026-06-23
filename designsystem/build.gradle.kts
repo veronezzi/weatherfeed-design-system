e1354c806784326892a9a737f112639b4b643c0c
veronezzi <veronezzi14@gmail.com>
2026-06-22 23:31:47 -0300

    build: add maven-publish for JitPack distribution

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
+    `maven-publish`
 }
 
 android {
@@ -29,6 +30,12 @@ android {
     buildFeatures {
         compose = true
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -39,4 +46,17 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.compose.ui.tooling)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId    = "com.github.veronezzi"
+                artifactId = "weatherfeed-design-system"
+                version    = "1.0.0"
+            }
+        }
+    }
 }
